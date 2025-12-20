ab5aefd1536eebe5b8d7682bd438460fff98d8d0
Maksym <mfedorenko16@gmail.com>
2025-12-20 18:11:02 -0500

    Implement home page with TradingView widget

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -18,9 +18,9 @@ const NavItems = () => {
   return (
     <ul className="flex flex-col sm:flex-row p-2 gap-3 sm:gap-10 font-medium">
       {NAV_ITEMS.map((item) => (
-          <li key={item.title}>
+          <li key={item.href}>
             <Link href={item.href} className={`hover:text-yellow-500 transition-colors ${isActive(item.href) ? 'text-gray-100' : ''}`}>
-              {item.title}
+              {item.label}
             </Link>
           </li>
       ))}
